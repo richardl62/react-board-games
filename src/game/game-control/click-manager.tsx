3225e86b60f5431e262b689e1d85812dab315933
slightly-nerdy <github@slightlynerdy.org>
2021-01-15 09:24:03 +0000

    Move ClickManager to new file + small change

---
+++ src/game/game-control/click-manager.tsx
@@ -0,0 +1,55 @@
+import { PiecePosition } from '../../interfaces';
+import { SquareProperties } from './game-control';
+
+export interface ClickManagerProps {
+    getSelectedSquare: () => PiecePosition | null;
+    setSelectedSquare: (arg: PiecePosition | null) => void;
+    doMove: (from: PiecePosition, to: PiecePosition | null) => void;
+}
+;
+
+export class ClickManager {
+    // A piece that has been selected by a first click and is available to
+    // move on a second click.
+    private _props: ClickManagerProps;
+
+    constructor(cmProps: ClickManagerProps) {
+        this._props = cmProps;
+
+        //console.log("CM Constructor:", props(this.selected));
+    }
+
+    get selected() {
+        return this._props.getSelectedSquare();
+    }
+
+    private set _selected(val: PiecePosition | null) {
+        //console.log("CM set: old", props(this.selected) , "new", props(val));
+        this._props.setSelectedSquare(val);;
+    }
+
+    clicked(pos: PiecePosition, squareProperties: SquareProperties) {
+        // console.log("CM clicked: selected", props(this.selected), "clicked", props(pos));
+        if (this.selected) {
+            if (PiecePosition.same(this.selected, pos)) {
+                // This same square has been clicked twice. Cancel the first click.
+                this._selected = null;
+            } else if (squareProperties.changeable) {
+                this._props.doMove(this.selected, pos);
+            } else {
+                this._selected = pos;
+            }
+        } else {
+            // No square is currently selected.
+            if (!squareProperties.pieceName) {
+                // An empty square is clicked with nothing selected. Do nothing.
+            } else {
+                this._selected = pos;
+            }
+        }
+    }
+
+    clear() {
+        this._selected = null;
+    }
+}
--- src/game/game-control/game-control.tsx
+++ src/game/game-control/game-control.tsx
@@ -4,6 +4,7 @@ import { GameDefinition, PiecePosition, PieceName}
     from '../../interfaces';
 
 import * as Bgio from '../../bgio';
+import { ClickManager } from './click-manager';
 
 const topLeftBlack = false; // KLUDGE
 
@@ -30,57 +31,6 @@ interface SquareProperties {
         canMoveTo: boolean;
     }
 }
-
-interface ClickManagerProps {
-    getSelectedSquare: () => PiecePosition | null;
-    setSelectedSquare: (arg: PiecePosition | null) => void;
-    doMove: (from: PiecePosition, to: PiecePosition | null) => void;
-}
-class ClickManager {
-    // A piece that has been selected by a first click and is available to
-    // move on a second click.
-    private _props: ClickManagerProps;
-
-    constructor(cmProps: ClickManagerProps) {
-        this._props = cmProps;
-
-        //console.log("CM Constructor:", props(this.selected));
-    }
-
-    get selected() {
-        return this._props.getSelectedSquare();
-    }
-
-    private set _selected(val : PiecePosition| null) {
-        //console.log("CM set: old", props(this.selected) , "new", props(val));
-
-        this._props.setSelectedSquare(val);;
-    }
-    
-    clicked(pos: PiecePosition, squareProperties: SquareProperties) {
-        // console.log("CM clicked: selected", props(this.selected), "clicked", props(pos));
-        if (this.selected) {
-            if(PiecePosition.same(this.selected, pos)) {
-                // This same square has been clicked twice. Cancel the first click.
-                this._selected = null;
-            } else {
-                this._props.doMove(this.selected, pos);
-                this._selected = null;
-            }
-        } else {
-            // No square is currently selected.
-            if (!squareProperties.pieceName) {
-                // An empty square is clicked with nothing selected. Do nothing.
-            } else {
-                this._selected = pos;
-            }
-        }
-    }
-
-    clear() {
-        this._selected = null;
-    }
-};
 
 class GameControl {
 
@@ -209,4 +159,4 @@ class GameControl {
 }
 
 export {GameControl as default, useGameControlProps};
-export type {SquareBackground}
+export type {SquareProperties, SquareBackground}
