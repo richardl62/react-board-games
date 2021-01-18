10bd1c498a8c2b7f4802252bde92b4c7be48d40b
slightly-nerdy <github@slightlynerdy.org>
2021-01-18 10:19:34 +0000

    Add simple server Intended to help with testing

---
+++ simple-server.ts
@@ -0,0 +1,14 @@
+// Adapted from https://boardgame.io/documentation/#/deployment?id=heroku
+import { Server } from 'boardgame.io/server';
+const gameDefinitions = require('./src/game-definition').default;
+const makeBgioGame = require('./src/bgio').makeGame;
+const bgioGames = gameDefinitions.map(makeBgioGame);
+
+// @ts-ignore
+console.log("Games: ", bgioGames.map(g => g.name));
+
+
+// @ts-ignore
+const server = Server({ games: bgioGames });
+
+server.run(8000);
