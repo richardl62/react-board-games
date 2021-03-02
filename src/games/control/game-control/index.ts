53f6f3d1a38195957abb538c4ba0061dbda43590
slightly-nerdy <github@slightlynerdy.org>
2021-03-02 11:48:10 +0000

    Rename 'layout' as 'render'

--- src/games/control/game-control/index.ts
+++ src/games/control/game-control/index.ts
@@ -1,4 +1,3 @@
 export { default, useGameControlProps, LegalMoveStatus  } from './game-control';
 export type { SquareProperties, SquareBackground } from './game-control'
-export { usePieceControl, useSquareControl } from '../../layout/drag-support';
 
