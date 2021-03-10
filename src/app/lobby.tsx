4be9d121907dbcf41aac50eb07b8a7b2af2b6527
slightly-nerdy <github@slightlynerdy.org>
2021-03-10 10:26:20 +0000

    Faff with Lobby

--- src/app/lobby.tsx
+++ src/app/lobby.tsx
@@ -5,25 +5,32 @@ import {Game} from './game';
 import {nonNull} from '../tools';
 import styles from './app.module.css';
 
-function lobbyItems(game: OnlineGame) {
+
+function makeGridItems(onlineGame: OnlineGame) {
     return [
-        <a href={game.address}>{game.id}</a>,
-        <span>({game.name})</span>,
-        <span>{game.status}</span>,
+        <a href={onlineGame.address}>{onlineGame.id}</a>,
+        <span>({onlineGame.name})</span>,
+        <span>{onlineGame.status}</span>,
     ];
 }
 
 function Lobby({ games }: {games: Array<Game>}) {
+    const names = games.map(g => g.name);
+
     const {servers} = useOptionsContext();
+    const onlineGames_= onlineGames(servers).filter(g => names.includes(g.name)); 
+
+    const gridItems = onlineGames_.map(makeGridItems);
 
     return (
         <div className={nonNull(styles.lobby)}>
             <h2>I'm prentending to be a lobby</h2>
             <div className={nonNull(styles.lobbyGrid)}>
-                {onlineGames(servers).map(lobbyItems).flat()}
+                {gridItems.flat()}
             </div>
         </div>
     );
 }
+
 
 export default Lobby;
