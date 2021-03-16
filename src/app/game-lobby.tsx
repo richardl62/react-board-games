5926e5dc94069dfac6037e0a164fa9a44827f7c2
slightly-nerdy <github@slightlynerdy.org>
2021-03-16 10:31:30 +0000

    WIp on game lobby

--- src/app/game-lobby.tsx
+++ src/app/game-lobby.tsx
@@ -1,88 +1,96 @@
 import { useState } from 'react';
 import { Game } from './types';
 import { Match, MatchList, CreatedMatch, useLobbyClient } from './lobby-client'
-// import { matchPath } from '../url-tools'
 import { nonNull } from '../tools';
 import styles from './app.module.css';
-//import { fileURLToPath } from 'url';
-
-
-
-// // When includeNames is false, the games are returned as empty spans.
-// function makeGridItems(game: Game, matches: Array<Match>) {
-//     let items : Array<JSX.Element> = [];
-//     const key = () => items.length;
-
-//     for(const index in matches) {
-//         const match = matches[index];
-//         const address = matchPath(game.name, match.matchID);
-//         items.push(<a href={address} key={key()}>{match.matchID}</a>);;
-//     };
-
-//     return items;
-// }
+import { matchPath } from '../url-tools';
 
 interface OnlineMatches {
     unset?: true;
     waiting?: true;
     error?: Error;
     matches?: Array<Match>;
- }; 
+};
 
-interface LobbyProps {
+interface MatchLinkProps {
+    game: Game;
+    match: Match;
+}
+function MatchLink({ game, match }: MatchLinkProps) {
+    const path = matchPath(game.name, match.matchID);
+    return (<div>
+        <a href={path}>{match.matchID}</a>
+    </div>
+    );
+};
+interface GameListProps {
     game: Game;
     onlineMatches: OnlineMatches;
 }
 
-function Lobby({onlineMatches}: LobbyProps) {
-    if(onlineMatches.unset) {
-        return <div>[Unset]</div>;
-    } else if(onlineMatches.waiting) {
-        return <div>...</div>;
-    } else if(onlineMatches.error) {
-        return <div>{"Error: " + onlineMatches.error.message}</div>;
-    } else {
-        const matchesDivs = onlineMatches.matches!.map((match, index) =>
-            <div key={index}>{match.matchID}</div>
-        );
-        return <div>{matchesDivs}</div>
+function GameList({ game, onlineMatches }: GameListProps) {
+    if (onlineMatches.unset) {
+        return null;
     }
+
+    if (onlineMatches.waiting) {
+        return <div>Waiting for server ...</div>;
+    }
+
+    if (onlineMatches.error) {
+        return <div>{'Error: ' + onlineMatches.error.message}</div>
+    }
+
+
+    const matches = onlineMatches.matches!;
+
+    return (
+        <>
+            <h1>Online Matches</h1>
+            <div>
+                {(matches.length === 0) ? 'No matches found' : null}
+                {matches.map((match, index) => 
+                    <MatchLink game={game} match={match} key={index} />
+                )}
+            </div>
+        </>
+    );
 }
 
 function GameLobby({ game }: { game: Game }) {
     const numPlayers = 1; // KLUDGE
     const lobbyContext = useLobbyClient();
-    const [ onlineMatches, setOnlineMatches ] = useState<OnlineMatches>({unset:true});
+    const [onlineMatches, setOnlineMatches] = useState<OnlineMatches>({ unset: true });
 
-    const refresh = () => {
-        setOnlineMatches({waiting:true});
+    const listMatches = () => {
+        setOnlineMatches({ waiting: true });
         lobbyContext.listMatches(game).then(
             (matchList: MatchList) => setOnlineMatches(matchList)
-        ).catch((error: Error) => setOnlineMatches({error: error}));
+        ).catch((error: Error) => setOnlineMatches({ error: error }));
     }
 
-    const recordCreatedMatch = (createdMatch: CreatedMatch) => {
-        let matches = onlineMatches.matches || [];
-        //matches.push(createdMatch);
-        setOnlineMatches({matches: matches});
+    const startCreatedMatch = (createdMatch: CreatedMatch) => {
+        const { matchID } = createdMatch;
+        const address = matchPath(game.name, matchID);
+        window.location.href = address;
     };
 
-    const newGame = () => {
-        setOnlineMatches({waiting:true});
+    const newMatch = () => {
+        setOnlineMatches({ waiting: true });
         lobbyContext.createMatch(game, numPlayers).then(
-            (createdMatch: CreatedMatch) => recordCreatedMatch(createdMatch)
-        ).catch((error: Error) => setOnlineMatches({error: error}));
+            (createdMatch: CreatedMatch) => startCreatedMatch(createdMatch)
+        ).catch((error: Error) => setOnlineMatches({ error: error }));
     }
 
     return (
         <div className={nonNull(styles.gameLobby)}>
-            <h1>Online Games</h1>
-            <Lobby game={game} onlineMatches={onlineMatches} />
-            <button type='button' onClick={newGame}>New Game</button>
-            <button type='button' onClick={refresh}>Refresh</button>
+            <div className={nonNull(styles.gameLobbyButtons)}>
+                <button type='button' onClick={newMatch}>New Match</button>
+                <button type='button' onClick={listMatches}>List Matches</button>
+            </div>
+            <GameList game={game} onlineMatches={onlineMatches} />
         </div>
     );
 }
-
 
 export { GameLobby };
