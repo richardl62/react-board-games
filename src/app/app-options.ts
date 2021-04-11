6824501a0267ae07c6ab58123c46e8588afd392c
Richard L <richard.latham@yahoo.co.uk>
2021-04-11 10:14:43 +0100

    fix setMatchID

--- src/app/app-options.ts
+++ src/app/app-options.ts
@@ -43,15 +43,24 @@ function getMatchID(usp: URLSearchParams) {
   return str;
 }
 
-
 const sp = new URLSearchParams(window.location.search);
 export const playersPerBrowser: number =  getPlayersPerBrowser(sp);
 export const bgioDebugPanel: boolean = getBgioDebugPanel(sp);
 export const matchIDFromURL: string|null = getMatchID(sp);
  
-export function setMatchID(matchID: string) {
-  console.error("Not yet implemented");
-}
+
 if (sp.toString()) {
   console.log("Unrecongised url parameters", sp.toString())
+}
+
+export function setMatchID(matchID: string) {
+  let url = new URL(window.location.href);
+  let seachParams = new URLSearchParams(url.search);
+  if(seachParams.has('matchID')) {
+    throw new Error("matchID is already set");
+  }
+  seachParams.set('matchID', matchID);
+  url.search = seachParams.toString();
+  
+  window.location.href = url.href;
 }
