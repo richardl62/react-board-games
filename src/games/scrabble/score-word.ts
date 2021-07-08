d1b5d31d26511f599e6b7ae1e8b42c1fbbad5dcf
Richard L <richard.latham@yahoo.co.uk>
2021-07-08 12:00:33 +0100

    Score words properly

---
+++ src/games/scrabble/score-word.ts
@@ -0,0 +1,35 @@
+import assert from "assert";
+import { squareTypesArray } from "./board-properties";
+import { BoardData } from "./game-data";
+import { letterScore } from "./letter-properties";
+import { multipliers } from "./square-type";
+
+interface RowCol {
+    row: number;
+    col: number;
+}
+
+export function scoreWord(board: BoardData, word: RowCol[]) {
+    let score = 0;
+    let wordMult = 1;
+
+    word.forEach(rc => {
+        const sq = board[rc.row][rc.col];
+        assert(sq);
+
+        const mults = multipliers(squareTypesArray[rc.row][rc.col]);
+
+        score += letterScore(sq.letter) * mults.letter;
+        wordMult *= mults.word;
+    })
+
+    return score * wordMult;
+}
+
+export function scoreWords(board: BoardData, words: RowCol[][]) : number {
+    let score = 0;
+    words.forEach(word => {score += scoreWord(board, word)});
+
+    return score;
+}
+
