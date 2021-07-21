e38cdb857a43be191c9d0c035b5f6e8869593614
Richard L <richard.latham@yahoo.co.uk>
2021-07-21 15:42:47 +0100

    Refactor end-turn confirmation

--- src/games/scrabble/turn-control.tsx
+++ src/games/scrabble/turn-control.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import styled from "styled-components";
 import { Bgio } from "../../shared/types";
 import { ClientMoves } from "./bgio-moves";
-import { EndTurnConfirmation } from "./end-turn-confirmation";
 import { findCandidateWords } from "./find-candidate-words";
 import { getWord } from "./game-actions";
 import { GameData } from "./game-data";
@@ -19,10 +18,21 @@ const OuterDiv = styled.div`
   margin-right: 0.6em;
 `;
 
+const IllegalWords = styled.div`
+  display: inline-flex;
+  gap: 0.5em;
+  font-size: large;
+  margin-bottom: 0.2em;
+  font-weight: bold;
+  color: darkred;
+`;
+
 export function TurnControl(props: Bgio.BoardProps<GameData>) {
   const board = props.G.board;
   const moves = props.moves as any as ClientMoves;
   const candidtateWords = findCandidateWords(board);
+
+  const [reportIllegalWords, setReportIllegalWords] = useState(false);
 
   if (candidtateWords === 'empty') {
     return <OuterDiv>
@@ -37,11 +47,41 @@ export function TurnControl(props: Bgio.BoardProps<GameData>) {
   }
 
   const score = scoreWords(board, candidtateWords);
+  const words = candidtateWords.map(cw => getWord(board, cw));
+  const illegalWords = words.filter(word => !isLegalWord(word));
 
-  return (
-    <OuterDiv>
-      <div>{scoreMessage + score}</div>
-      <button onClick={() => moves.finishTurn(score)}> done </button>
-    </OuterDiv>
-  );
+  if (illegalWords.length > 0 && reportIllegalWords) {
+    const onClick = () => {
+        setReportIllegalWords(false);
+        moves.finishTurn(score);
+    }
+
+    return (
+      <div>
+        <IllegalWords>
+          Unrecognised Words:
+          {illegalWords.map(w => <span key={w}>{w.toLowerCase()}</span>)}
+        </IllegalWords>
+
+        <OuterDiv>
+          <div>{scoreMessage + score}</div>
+          <button onClick={onClick}> Done (allow unrecongised words) </button>
+        </OuterDiv>
+      </div>
+    )
+  } else {
+    const onClick = () => {
+        if(illegalWords.length > 0) {
+          setReportIllegalWords(true);
+        } else {
+          moves.finishTurn(score);
+        }
+    }
+    return (
+      <OuterDiv>
+        <div>{scoreMessage + score}</div>
+        <button onClick={onClick}> done </button>
+      </OuterDiv>
+    );
+  }
 }
