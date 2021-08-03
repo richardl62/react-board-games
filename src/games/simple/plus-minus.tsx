3bd93a2ca740e454b58529fae42e23ce4fcb9b0f
Richard L <richard.latham@yahoo.co.uk>
2021-08-03 09:22:40 +0100

    Make plusminus more complex

--- src/games/simple/plus-minus.tsx
+++ src/games/simple/plus-minus.tsx
@@ -5,23 +5,28 @@ import { AppGame, BoardProps } from '../../shared/types';
 import styled from 'styled-components';
 import { WaitingForPlayers } from '../../game-support/waiting-for-players';
 
+// Needlessly complex to help with testing. (Hmm, that seems to be contradict itself.)
 interface G {
-  value: number;
+  data: {
+    dummy: string;
+    count: number;
+  }
+  dummy: string;
 };
 
-const Name = styled.span<{toPlay: boolean}>`
+const Name = styled.span<{ toPlay: boolean }>`
   text-decoration: ${props => props.toPlay ? 'underline' : 'none'}
-` 
+`
 function PlayerData(props: BoardProps<G>) {
   const playerSpan = (id: string) => {
-    const {status, name} = props.playerData[id];
+    const { status, name } = props.playerData[id];
     const isActive = id === props.playerID;
-    const toPlay = id===props.ctx.currentPlayer;
+    const toPlay = id === props.ctx.currentPlayer;
 
     return (
       <span key={name}>
         <Name toPlay={toPlay}>
-          {name + (isActive? " (you)" : "")}
+          {name + (isActive ? " (you)" : "")}
         </Name>
         <span>
           {(status !== 'ready') && ` (${status})`}
@@ -38,10 +43,10 @@ function PlayerData(props: BoardProps<G>) {
 
 function Board(props: BoardProps<G>) {
 
-  if(!props.allJoined) {
+  if (!props.allJoined) {
     return <WaitingForPlayers {...props} />
   }
-  
+
   const { G, moves, events } = props
   return (
     <div>
@@ -51,24 +56,32 @@ function Board(props: BoardProps<G>) {
         <button type="button" onClick={(() => moves.add(1))}>+1</button>
         <button type="button" onClick={(() => moves.add(-1))}>-1</button>
         <button type="button" onClick={() => events.endTurn!()}>End Turn</button>
-        <div>{G.value}</div>
+        <div>{G.data.count}</div>
       </div>)}
     </div>
-    );
+  );
 }
 
-export const plusminus : AppGame = {
+export const plusminus: AppGame = {
   name: 'plusminus',
   displayName: 'Plus Minus (for testing)',
 
-  setup: (): G => { return { value: 0 }; },
+  setup: (): G => {
+    return {
+      data: {
+        dummy: "dummy string",
+        count: 0,
+      },
+      dummy: "another dummy string",
+    };
+  },
 
   minPlayers: 1,
   maxPlayers: 100,
 
   moves: {
     add: (G: G, ctx: Ctx, value: number) => {
-      G.value += value;
+      G.data.count += value;
     },
   },
 
