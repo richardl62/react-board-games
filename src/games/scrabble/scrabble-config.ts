3000c3ae673382f596b655c0c3b01ed79576afc2
Richard L <richard.latham@yahoo.co.uk>
2021-08-05 10:08:30 +0100

    Allow customised Scrabble games (part 1)

--- src/games/scrabble/scrabble-config.ts
+++ src/games/scrabble/scrabble-config.ts
@@ -1,5 +1,7 @@
 import { gAssert } from "../../shared/assert";
 import { SquareType } from "./square-type";
+
+// KLUDGE?: For now at least, all scrabble configurations use the same letter scores.
 const letterScores = {
     '?': 0,
     A: 1, E: 1, I:1, L:1, N:1, O:1, R:1, S:1, T:1, U:1,
@@ -11,27 +13,14 @@ const letterScores = {
     Q:10, Z: 10,
 }
 Object.freeze(letterScore);
-
 export type Letter = keyof typeof letterScores;
+
+// KLUDGE: See comment on letterScores
+export const allLetterBonus = 50;
 
 export function letterScore(l: Letter) : number {
     return letterScores[l];
 }
-
-const letterDistribution = {
-    A:9, B:2, C:2, D:4, E:12, F:2, G:3, H:2, I:9, J:1, K:1, L:4, M:2, N:6, O:8, P:2, Q:1, R:6, S:4, T:6, U:4, V:2, W:2, X:1, Y:2, Z:1, '?':2, 
-};
-Object.freeze(letterDistribution);
-
-export let fullBag: Array<Letter> = [];
-for(const letter_ in letterDistribution) {
-    const letter = letter_ as keyof typeof letterDistribution;
-    const count = letterDistribution[letter];
-    for(let i = 0; i < count; ++i) {
-        fullBag.push(letter);
-    }
-} 
-Object.freeze(fullBag);
 
 const D = SquareType.doubleWord;
 const T = SquareType.tripleWord;
@@ -39,34 +28,49 @@ const d = SquareType.doubleLetter;
 const t = SquareType.tripleLetter;
 const s = SquareType.simple;
 
-export const squareTypesArray = [
-    [T, s, s, d, s, s, s, T, s, s, s, d, s, s, T],
-    [s, D, s, s, s, t, s, s, s, t, s, s, s, D, s],
-    [s, s, D, s, s, s, d, s, d, s, s, s, D, s, s],
-    [d, s, s, D, s, s, s, d, s, s, s, D, s, s, d],
-    [s, s, s, s, D, s, s, s, s, s, D, s, s, s, s],
-    [s, t, s, s, s, t, s, s, s, t, s, s, s, t, s],
-    [s, s, d, s, s, s, d, s, d, s, s, s, d, s, s],
-    [T, s, s, d, s, s, s, D, s, s, s, d, s, s, T],
-    [s, s, d, s, s, s, d, s, d, s, s, s, d, s, s],
-    [s, t, s, s, s, t, s, s, s, t, s, s, s, t, s],
-    [s, s, s, s, D, s, s, s, s, s, D, s, s, s, s],
-    [d, s, s, D, s, s, s, d, s, s, s, D, s, s, d],
-    [s, s, D, s, s, s, d, s, d, s, s, s, D, s, s],
-    [s, D, s, s, s, t, s, s, s, t, s, s, s, D, s],
-    [T, s, s, d, s, s, s, T, s, s, s, d, s, s, T],
-];
-Object.freeze(squareTypesArray);
+export const standard = {
+    letterDistribution: {
+        A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, 
+        I: 9, J: 1, K: 1, L: 4, M:  2, N: 6, O: 8, P: 2, 
+        Q: 1, R: 6, S: 4, T: 6, U:  4, V: 2, W: 2, X: 1, Y: 2, Z: 1, '?': 2,
+    },
 
-export const rackSize = 7;
+    boardLayout: [
+        [T, s, s, d, s, s, s, T, s, s, s, d, s, s, T],
+        [s, D, s, s, s, t, s, s, s, t, s, s, s, D, s],
+        [s, s, D, s, s, s, d, s, d, s, s, s, D, s, s],
+        [d, s, s, D, s, s, s, d, s, s, s, D, s, s, d],
+        [s, s, s, s, D, s, s, s, s, s, D, s, s, s, s],
+        [s, t, s, s, s, t, s, s, s, t, s, s, s, t, s],
+        [s, s, d, s, s, s, d, s, d, s, s, s, d, s, s],
+        [T, s, s, d, s, s, s, D, s, s, s, d, s, s, T],
+        [s, s, d, s, s, s, d, s, d, s, s, s, d, s, s],
+        [s, t, s, s, s, t, s, s, s, t, s, s, s, t, s],
+        [s, s, s, s, D, s, s, s, s, s, D, s, s, s, s],
+        [d, s, s, D, s, s, s, d, s, s, s, D, s, s, d],
+        [s, s, D, s, s, s, d, s, d, s, s, s, D, s, s],
+        [s, D, s, s, s, t, s, s, s, t, s, s, s, D, s],
+        [T, s, s, d, s, s, s, T, s, s, s, d, s, s, T],
+    ],
+
+    rackSize: 7,
+}
+Object.freeze(standard);
+
+export type ScrabbleConfig = typeof standard;
 
 // Sanity checks. (Could be debug-only)
 
 gAssert(Object.keys(letterScores).length === 27, "Problem with setup");
-gAssert(Object.keys(letterDistribution).length === 27, "Problem with setup");
-gAssert(fullBag.length === 100, "Problem with setup");
+gAssert(Object.keys(standard.letterDistribution).length === 27, "Problem with setup");
 
-gAssert(squareTypesArray.length === 15);
-squareTypesArray.forEach(row => gAssert(row.length === 15));
+//KLUDGE: Overly complex - using reduce just of the practice. 
+const letterCount : number = Object.entries(standard.letterDistribution).reduce(
+    (prevCount, ld) => prevCount + ld[1], 0
+)
+gAssert(letterCount === 100, "Problem with setup");
 
-export const allLetterBonus = 50;
+gAssert(standard.boardLayout.length === 15);
+standard.boardLayout.forEach(row => gAssert(row.length === 15));
+
+
