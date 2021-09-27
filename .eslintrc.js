6459d53182a74400fc650a3ca0a57240538a0359
Richard L <richard.latham@yahoo.co.uk>
2021-09-27 10:25:02 +0100

    Rerun npx eslint --init

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+/* eslint-env node */
+module.exports = {}; /* https://github.com/eslint/eslint/issues/13008 */
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
