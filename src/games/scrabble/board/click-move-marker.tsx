7297e4aa429dd93f1def7fc27b0bf1f72679e175
Richard L <richard.latham@yahoo.co.uk>
2021-11-19 19:22:21 +0000

    Add crude arrows as click start markers

--- src/games/scrabble/board/click-move-marker.tsx
+++ src/games/scrabble/board/click-move-marker.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import { ClickMoveDirection } from "../actions/local-game-state";
 
+const ArrowDiv = styled.div`
+    height: 100%;
+    width: 100%;
+    
+    font-size: 100%;
+    font-weight: 900;
+`;
 
 interface ClickMoveMarkerProps {
     direction: ClickMoveDirection;
 }
 
 export function ClickMoveMarker(props: ClickMoveMarkerProps) : JSX.Element {
-    return <div>{props.direction}</div>;
+    return props.direction === "right" ?
+        <ArrowDiv>&rarr;</ArrowDiv> : <ArrowDiv>&darr;</ArrowDiv>;
 }
