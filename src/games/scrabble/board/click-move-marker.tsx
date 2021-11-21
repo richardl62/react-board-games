75b8fd7b79f0b9e58787dc7a86bff43e87872731
Richard L <richard.latham@yahoo.co.uk>
2021-11-21 14:49:08 +0000

    Remove svg file to avoid build problems tsc did not copy the svg file to dist

--- src/games/scrabble/board/click-move-marker.tsx
+++ src/games/scrabble/board/click-move-marker.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import { ClickMoveDirection } from "../actions/local-game-state";
 import { squareSize } from "./style";
-import {ReactComponent as Arrow} from "./216093_right_arrow_icon.svg";
 const OuterDiv = styled.div<{rotation: string}>`
 
     transform:rotate(${props => props.rotation});         /* Standard syntax */
@@ -17,6 +16,6 @@ interface ClickMoveMarkerProps {
 export function ClickMoveMarker(props: ClickMoveMarkerProps) : JSX.Element {
     console.log(props);
     return <OuterDiv rotation={props.direction === "down" ? "90deg" : "none" } >
-        <Arrow  height={"100%"}  width={"100%"}/>
+        {props.direction}
     </OuterDiv>;
 }
