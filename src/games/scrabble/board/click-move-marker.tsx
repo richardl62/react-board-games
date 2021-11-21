61ac5cdeda0d16fba215e7c9c8d93972ce62c78b
Richard L <richard.latham@yahoo.co.uk>
2021-11-21 15:06:14 +0000

    Implement click-move-marker arrow is CSS
    Having it as SVG caused prolems with heroku build as tsc did not
    copy the svg file to dist.

--- src/games/scrabble/board/click-move-marker.tsx
+++ src/games/scrabble/board/click-move-marker.tsx
@@ -3,11 +3,26 @@ import styled from "styled-components";
 import { ClickMoveDirection } from "../actions/local-game-state";
 import { squareSize } from "./style";
 const OuterDiv = styled.div<{rotation: string}>`
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
     transform:rotate(${props => props.rotation});         /* Standard syntax */
+
     height: ${squareSize};
     width: ${squareSize};
+
+    
 `;
+
+const ArrowHead = styled.div`
+
+    /* Using 3 borders gives as arrow head */
+    border-top: calc(${squareSize}*0.3) solid transparent;
+    border-left: calc(${squareSize}*0.6) solid #77a81c;
+    border-bottom: calc(${squareSize}*0.3) solid transparent;
+`;
+       
 
 interface ClickMoveMarkerProps {
     direction: ClickMoveDirection;
@@ -16,6 +31,6 @@ interface ClickMoveMarkerProps {
 export function ClickMoveMarker(props: ClickMoveMarkerProps) : JSX.Element {
     console.log(props);
     return <OuterDiv rotation={props.direction === "down" ? "90deg" : "none" } >
-        {props.direction}
+        <ArrowHead />
     </OuterDiv>;
 }
