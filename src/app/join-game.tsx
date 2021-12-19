e6783a6f03247958c42cdc1c5a48ddd0174fb30f
Richard L <richard.latham@yahoo.co.uk>
2021-12-19 13:43:38 +0000

    Add placeholder MatchLobby

---
+++ src/app/join-game.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useAsyncCallback } from "react-async-hook";
+import { joinMatch } from "../bgio";
+import { AppGame, MatchID } from "../shared/types";
+import { WaitingOrError, waitingOrError } from "../shared/waiting-or-error";
+import { addPlayerToHref } from "./url-params";
+
+interface JoinGameProps {
+    game: AppGame;
+    matchID: MatchID;
+}
+
+/**
+ * For now at least, GameLobby just allows a player to join
+ */
+export function JoinGame(props: JoinGameProps): JSX.Element {
+    const { game, matchID } = props;
+    const [name, setName] = useState<string>("");
+    
+    const joinGameCallback = useAsyncCallback(() =>
+        joinMatch(game, matchID, name).then(player => {
+            const newHref = addPlayerToHref(player);
+            window.location.href = newHref;
+        })
+    );
+
+    if(waitingOrError(joinGameCallback)) {
+        return <WaitingOrError status={joinGameCallback} />;
+    }
+
+    return <div>
+        <label>Name</label>
+        <input
+            value={name}
+            placeholder='Player name'
+            onInput={e => setName(e.currentTarget.value)}
+        />
+
+        <button
+            type="button"
+            onClick={joinGameCallback.execute}
+            disabled={joinGameCallback.loading}
+        >
+            Join
+        </button>
+    </div>;
+}
+  
--- src/app/match-lobby.tsx
+++ src/app/match-lobby.tsx
@@ -1,9 +1,6 @@
-import React, { useState } from "react";
-import { useAsyncCallback } from "react-async-hook";
-import { joinMatch } from "../bgio";
+import React from "react";
 import { AppGame, MatchID } from "../shared/types";
-import { WaitingOrError, waitingOrError } from "../shared/waiting-or-error";
-import { addPlayerToHref } from "./url-params";
+import { JoinGame } from "./join-game";
 
 interface MatchLobbyProps {
     game: AppGame;
@@ -15,34 +12,6 @@ interface MatchLobbyProps {
  */
 export function MatchLobby(props: MatchLobbyProps): JSX.Element {
     const { game, matchID } = props;
-    const [name, setName] = useState<string>("");
-    
-    const joinGameCallback = useAsyncCallback(() =>
-        joinMatch(game, matchID, name).then(player => {
-            const newHref = addPlayerToHref(player);
-            window.location.href = newHref;
-        })
-    );
 
-    if(waitingOrError(joinGameCallback)) {
-        return <WaitingOrError status={joinGameCallback} />;
-    }
-
-    return <div>
-        <label>Name</label>
-        <input
-            value={name}
-            placeholder='Player name'
-            onInput={e => setName(e.currentTarget.value)}
-        />
-
-        <button
-            type="button"
-            onClick={joinGameCallback.execute}
-            disabled={joinGameCallback.loading}
-        >
-            Join
-        </button>
-    </div>;
+    return <JoinGame game={game} matchID={matchID} />;
 }
-  
