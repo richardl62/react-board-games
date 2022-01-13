d36cdba56cf62ae5fcfb5173ae23ffddb60626b7
Richard L <richard.latham@yahoo.co.uk>
2022-01-13 08:59:11 +0000

    Add AsyncStatus

--- src/shared/waiting-or-error.tsx
+++ src/shared/waiting-or-error.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 
+const Message = styled.div`
+    font-size: large;
+`;
+
 const Waiting = styled.div`
     font-size: large;
 `;
@@ -16,6 +20,11 @@ const ErrorMessage = styled(Waiting)`
 interface AsyncStatus {
     loading?: boolean; // 'loading' rather than 'waiting' to suit useAsync.
     error?: Error;
+    result?: unknown;
+}
+
+function nDefined(elems: unknown[]) {
+    return elems.filter(e => e !== undefined).length;
 }
 
 export function waitingOrError(status: AsyncStatus) : boolean {
@@ -50,4 +59,35 @@ export function WaitingOrError(props: WaitingOrErrorProps) : JSX.Element | null 
     }
 
     return null;
+}
+
+interface AsyncStatusProps {
+    status: AsyncStatus;
+    /** Optional name of activity. Used in error and waiting messages */ 
+    activity: string;
+}
+
+export function AsyncStatus(props: AsyncStatusProps) : JSX.Element | null {
+    const { status: {loading, error, result}, activity } = props;
+
+    let message = "unexpected async status";
+    if(nDefined([loading, error, result]) === 1) {
+        if(loading) {
+            message = "loading ...";
+        }
+
+        if(error) {
+            message = "ERROR: " + error.message;
+        }
+
+        if(result !== undefined) {
+            message = "result available";
+        }
+    }
+
+    return <Message>
+        <span>{activity + " : "}</span>
+        <span>{message}</span>
+    </Message>;
+
 }
