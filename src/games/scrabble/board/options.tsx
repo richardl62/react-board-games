de95a4b76dbecfe7219d2c9d02d53c9ce35bed39
Richard L <richard.latham@yahoo.co.uk>
2022-02-09 09:24:28 +0000

    Remove 'spunds' option

--- src/games/scrabble/board/options.tsx
+++ src/games/scrabble/board/options.tsx
@@ -1,28 +1,30 @@
-import React from "react";
-import styled from "styled-components";
-import { useScrabbleContext } from "./scrabble-context";
+// import React from "react";
+// import styled from "styled-components";
+// import { useScrabbleContext } from "./scrabble-context";
 
-const OptionsDiv = styled.div`
-    display: flex;
-    justify-content: right;
-    font-size: 14px;
-    label {
-        margin-left: 2px;
-    }    
-`;
+// const OptionsDiv = styled.div`
+//     display: flex;
+//     justify-content: right;
+//     font-size: 14px;
+//     label {
+//         margin-left: 2px;
+//     }    
+// `;
 
-export function Options() : JSX.Element {
-    const { soundsAllowed, dispatch } = useScrabbleContext();
+export function Options(): JSX.Element | null {
+    //     const { soundsAllowed, dispatch } = useScrabbleContext();
 
-    const onChange = () => {
-        dispatch({
-            type: "allowSounds", 
-            data: {allow: !soundsAllowed}
-        });
-    };
+    //     const onChange = () => {
+    //         dispatch({
+    //             type: "allowSounds", 
+    //             data: {allow: !soundsAllowed}
+    //         });
+    //     };
 
-    return <OptionsDiv>
-        <input type="checkbox" id="sounds" onChange={onChange} checked={soundsAllowed} />
-        <label htmlFor="sounds">sounds</label>
-    </OptionsDiv>;
+    //     return <OptionsDiv>
+    //         <input type="checkbox" id="sounds" onChange={onChange} checked={soundsAllowed} />
+    //         <label htmlFor="sounds">sounds</label>
+    //     </OptionsDiv>;
+    // 
+    return null;
 }
