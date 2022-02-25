d132327cbcf2984f35e2dee430ee5b5b1e177545
Richard L <richard.latham@yahoo.co.uk>
2022-02-25 16:51:26 +0000

    Wip of drag drop

--- src/games/cribbage/board/hand.tsx
+++ src/games/cribbage/board/hand.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+import { PieceHolder, PieceHolderStyle, DragDrop } from "../../../utils/board/piece-holder";
 import { Card, CardSVG } from "../../../utils/cards";
+import { cardSize } from "../../../utils/cards/styles";
 import { cardName } from "../../../utils/cards/types";
 
 type ShowBack = Parameters<typeof CardSVG>[0]["showBack"];
+
+const HandDiv = styled.div`
+    display: flex;
+`;
 
 interface Props {
     cards: (Card|null)[];
@@ -12,11 +19,32 @@ interface Props {
 
 export function Hand(props: Props) : JSX.Element {
     const { cards, showBack } = props;
-    return <div>
+
+    const style : PieceHolderStyle = {
+        ...cardSize, // for hieght and widht
+        background: {color: "white"},
+    };
+
+    const dragDrop : DragDrop = { 
+        /** Id of piece to drag. Used as parameter to onDrop.
+         */
+        id: {card: "card"},
+    
+        draggable: true,
+    
+
+        end: arg => {
+            console.log("Drag", arg.drag, "drop", arg.drop);
+        }
+    };
+
+    return <HandDiv>
         {cards.map(card => {
             const key = card ? cardName(card) : "empty";
 
-            return <CardSVG key={key} card={card} showBack={showBack} />;
+            return <PieceHolder key={key} style={style} dragDrop={dragDrop} >
+                <CardSVG  card={card} showBack={showBack} />
+            </PieceHolder>; 
         })}
-    </div>;
+    </HandDiv>;
 }
