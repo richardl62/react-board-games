4a3dfd00f0328a7b16624d8a39a2074e7dfc75f7
Richard L <richard.latham@yahoo.co.uk>
2022-06-03 15:02:02 +0100

    Wip showing scoring

--- src/games/crosstiles/board/grid-status.tsx
+++ src/games/crosstiles/board/grid-status.tsx
@@ -11,30 +11,40 @@ import { ScoreCard } from "../server-side/score-card";
 interface GridStatusProps {
     scoreCard: ScoreCard, 
     grid: (Letter | null)[][],
+    requestConfirmation?: boolean;
 }
 export function GridStatus(props: GridStatusProps) : JSX.Element | null {
 
-    const { scoreCard, grid } = props;
+    const { scoreCard, grid, requestConfirmation } = props;
     const { gridCategory, scoreAs} = findGridCategory(grid, scoreCard, null);
     const { isLegalWord } = useCrossTilesContext();
 
     let text = "No score";
-
+    let scoreAvailable = Boolean(gridCategory);
     if (gridCategory) {
         text = displayName[gridCategory];
         if (scoreAs === "chance") {
             text += " (available as chance)";
         } else if (scoreAs === null) {
             text += " (not available)";
+            scoreAvailable = false;
         }
 
         if (scoreAs) {
             const illegalWords = findIllegalWords(grid, isLegalWord);
             if (illegalWords) {
                 text = "Illegal word(s): " + illegalWords.join(" ");
+                scoreAvailable = false;
             }
         }
     }
 
-    return <div>{text}</div>;
+    const confirmation = scoreAvailable ?
+        "Player must confirm score" :
+        "Player must chose category to zero";
+
+    return <div>
+        <div>{text}</div>
+        {requestConfirmation && <div>{confirmation}</div>}
+    </div>;
 }
