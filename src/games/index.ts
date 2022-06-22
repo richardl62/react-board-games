bc7810f29345ac0720dbe925f024178629e094c0
Richard L <richard.latham@yahoo.co.uk>
2022-06-22 08:54:36 +0100

    Change an import path

--- src/games/index.ts
+++ src/games/index.ts
@@ -1,6 +1,6 @@
 import { AppGame } from "../app-game-support";
 import scrabble from "./scrabble";
-import crosstiles from "./crosstiles/app-game/app-game";
+import crosstiles from "./crosstiles";
 import plusMinus from "./plus-minus";
 import swapSquares from "./swap-squares";
 
@@ -9,5 +9,4 @@ export const games : Array<AppGame> = [
     ...crosstiles,   
     ...plusMinus,
     ...swapSquares,
-  
 ];
