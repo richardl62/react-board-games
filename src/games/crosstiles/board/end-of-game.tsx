d0154139c99309bdc0bafeecc010d8b98e1db497
Richard L <richard.latham@yahoo.co.uk>
2022-07-12 19:06:48 +0100

    Changes to EndOfGame

--- src/games/crosstiles/board/end-of-game.tsx
+++ src/games/crosstiles/board/end-of-game.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { useCrossTilesContext } from "../client-side/actions/cross-tiles-context";
 import { totalScore } from "../server-side/score-card";
 import { GameStage } from "../server-side/server-data";
-import { PlayerStatus } from "./player-status";
+
 
 const GameOverDiv = styled.div`
     display: flex;
@@ -18,7 +18,7 @@ const Heading = styled.div`
 const ScoreTable = styled.div`
     display: inline-grid;
     column-gap: 0.5em;
-    grid-template-columns: auto auto;
+    grid-template-columns: auto auto auto;
     margin-bottom: 8px;
 `;
 
@@ -35,15 +35,14 @@ export function EndOfGame() : JSX.Element | null {
     for(const pid in playerData) {
         const name = getPlayerName(pid);
         const score = totalScore(playerData[pid].scoreCard);
-        scores.push({name,score});
+        const message = playerData[pid].readyForNewGame ? " (Ready for new game)" : "";
+        scores.push({name,score,message});
     }
     
     // Highest score first
     scores.sort((p1, p2) => p2.score - p1.score);
 
     const amReady =  playerData[playerID].readyForNewGame;
-    const readyMessage = (pid: string) =>
-        playerData[pid].readyForNewGame ? "Ready for new game" : null;
 
     return <GameOverDiv>
         <Heading>Final scores</Heading>
@@ -51,12 +50,12 @@ export function EndOfGame() : JSX.Element | null {
         <ScoreTable>
             {scores.map(ps => [
                 <div key={ps.name}>{ps.name}</div>,
-                <div key={ps.name+"score"}>{ps.score}</div>
+                <div key={ps.name+"score"}>{ps.score}</div>,
+                <div key={ps.name+"message"}>{ps.message}</div> 
             ]
             )}
         </ScoreTable>
 
         {!amReady && <button onClick={()=>moves.readyForNewGame()}>Start new game</button>}
-        <PlayerStatus message={readyMessage}/>
     </GameOverDiv>;
 }
