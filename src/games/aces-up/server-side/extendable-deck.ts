5d87a6da0b357787ab138e525fb034a8c334582b
Richard L <richard.latham@yahoo.co.uk>
2023-02-10 14:20:44 +0000

    Utils/AcesUp: Add and use drawNoJokers

--- src/games/aces-up/server-side/extendable-deck.ts
+++ src/games/aces-up/server-side/extendable-deck.ts
@@ -1,26 +1,26 @@
 import { Ctx } from "boardgame.io";
 import { sAssert } from "../../../utils/assert";
-import { Card } from "../../../utils/cards";
-import { deck as drawDeck } from "../../../utils/cards/deck";
+import { CardNonJoker } from "../../../utils/cards";
+import { deckNoJokers } from "../../../utils/cards/deck";
 
 type Option = "noKings";
 export class ExtendingDeck {
-    constructor(ctx: Ctx, deck: Card[], option?: Option) {
+    constructor(ctx: Ctx, deck: CardNonJoker[], option?: Option) {
  
         this.ctx = ctx;
         this.deck = deck;
         this.option = option || null;
     }
     ctx: Ctx;
-    deck: Card[];
+    deck: CardNonJoker[];
     option: Option | null;
 
-    draw() : Card {
+    draw() : CardNonJoker {
         let card;
         while (!card) {
             const c = this.deck.pop();
             if (!c) {
-                const newCards = drawDeck({ jokers: false });
+                const newCards = deckNoJokers();
 
                 sAssert(this.ctx.random);
                 this.deck.splice(this.deck.length, 0,
@@ -34,7 +34,7 @@ export class ExtendingDeck {
         return card;
     }
 
-    drawN(count: number) : Card[] {
+    drawN(count: number) : CardNonJoker[] {
         const cards = [];
         for(let i = 0; i < count; ++i) {
             cards.push(this.draw());
