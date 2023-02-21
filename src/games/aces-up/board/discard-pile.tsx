8da75e00b5498469dc117000f29398434f44065f
Richard L <richard.latham@yahoo.co.uk>
2023-02-21 14:18:21 +0000

    AcesUp: Change rendering of discard piles This give better dragging appearance better on Firefox.

--- src/games/aces-up/board/discard-pile.tsx
+++ src/games/aces-up/board/discard-pile.tsx
@@ -3,84 +3,66 @@ import React from "react";
 import styled from "styled-components";
 import { CardNonJoker, CardSVG } from "../../../utils/cards";
 import { cardSize, cardVerticalStackingOffset } from "../../../utils/cards/styles";
-import { cardName } from "../../../utils/cards/types";
-import { CardID } from "../game-control/card-id";
 import { useCardDragRef, useCardDropRef } from "./drag-drop";
 
-function cardTop(index: number) {
-    return index * cardVerticalStackingOffset;
+
+const SubPileDiv = styled.div`
+    position: absolute;
+
+    /* KLUDGE?: Set top on all but the first of the nested divs */
+    > div {
+        top: ${cardVerticalStackingOffset}px;
+    }
+`;
+
+function SubPile(props: {
+    cards: CardNonJoker[],
+    cardIndex: number,
+    pileIndex: number,
+    owner: PlayerID,
+}) {
+    const {cards, cardIndex, pileIndex, owner } = props;
+
+    const dragRef = useCardDragRef({area: "discardPileCard", pileIndex, cardIndex, owner});
+
+    return <SubPileDiv ref={dragRef}>
+        <CardSVG card={cards[cardIndex]} />
+        {cardIndex < cards.length - 1 &&
+            <SubPile cards={cards} cardIndex={cardIndex+1} pileIndex={pileIndex} owner={owner} />
+        }
+
+    </SubPileDiv>;
 }
 
 function pileHeight(nCards: number) {
     if(nCards === 0) {
         return cardSize.height;
     } else {
-        return cardSize.height + cardTop(nCards-1);
+        return cardSize.height + (nCards-1) * cardVerticalStackingOffset;
     }
 }
 
-const OuterDiv = styled.div<{nCards: number}>`
+const DiscardPileDiv = styled.div<{height: number}>`
     position: relative;
 
-    height: ${props => pileHeight(props.nCards)}px;
+    /* SubPile uses absolute position and so does not set size  */
+    height: ${props => props.height}px;
     width: ${cardSize.width}px;
 `;
-
-const CardAreaDiv = styled.div<{
-    cardsAbove: number,
-    cardsBelow: number,
-}>`
-    /* KLUDGE?: Rely on order of cards to get the required overlapping.
-      (The same overlap can achieved by setting z-index to cardsAbove, but then 
-      only the selected card would apoear to drag, not the cards below it. 
-      Limitation: With Firefox only the selected card appears to drag wether
-      or not the z-index is set.)*/
-    position: absolute;
-    top: ${props => cardTop(props.cardsAbove)}px;
-
-    height: ${props => pileHeight(props.cardsBelow)}px;
-`;
-
-function CardArea(props:{
-    cards: CardNonJoker[],
-    cardIndex: number,
-    cardID: CardID,
-}) {
-    const { cards, cardIndex, cardID } = props;
-    const dragRef = useCardDragRef(cardID);
-
-    // Card with index 0 is at the top
-    const cardsAbove = cardIndex;
-    const cardsBelow = cards.length - (cardIndex + 1);
-
-    return <CardAreaDiv ref={dragRef} cardsAbove={cardsAbove} cardsBelow={cardsBelow}>
-        <CardSVG card={cards[cardIndex]} />
-    </CardAreaDiv>;
-}
 
 export function DiscardPile(props: {
     cards: CardNonJoker[];
     index: number;
-    owner: PlayerID;
+    owner: PlayerID; 
 }): JSX.Element {
     const { cards, owner, index: pileIndex } = props;
 
     const dropRef = useCardDropRef({area: "discardPileAll", pileIndex, owner});
-    
-    const cardDivs : JSX.Element[] = [];
 
-    /* KLUDGE?: Backwards loop to achieve desired overlap - see CardDiv */
-    for(let cardIndex = 0; cardIndex < cards.length; ++cardIndex) {
-        const cardID : CardID = { area: "discardPileCard", pileIndex, cardIndex: cardIndex, owner };
-        const key= cardName(cards[cardIndex]) + cardIndex;
-
-        cardDivs.push(
-            <CardArea key={key} cards={cards} cardIndex={cardIndex} cardID={cardID} />
-        );
-    }
-
-    return <OuterDiv ref={dropRef} nCards={cards.length}>
-        {cardDivs.length === 0 && <CardSVG />}
-        {cardDivs}
-    </OuterDiv>;
+    return <DiscardPileDiv ref={dropRef} height={pileHeight(cards.length)}>
+        {cards.length === 0 ? 
+            <CardSVG /> : 
+            <SubPile cards={cards} cardIndex={0} pileIndex={pileIndex} owner={owner} />
+        }
+    </DiscardPileDiv>;
 }
