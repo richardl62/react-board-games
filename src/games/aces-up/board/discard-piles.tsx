36b08b97e98294e891536ddc2980a9262215aff1
Richard L <richard.latham@yahoo.co.uk>
2023-02-27 18:43:03 +0000

    AcesUp: Replace DiscardPile with CardStack

--- src/games/aces-up/board/discard-piles.tsx
+++ src/games/aces-up/board/discard-piles.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import { CardID } from "../game-control/card-id";
 import { usePlayerData } from "../game-support/game-context";
 import { columnGap } from "../game-support/styles";
 import { AreaLabelBelow } from "./area-label";
-import { DiscardPile } from "./discard-pile";
+import { CardStack } from "./card-stack";
 import { PlayerInfo } from "./player-info";
 
 const PilesDiv = styled.div`
@@ -17,13 +18,21 @@ interface Props {
 }
 
 export function Discards(props: Props) : JSX.Element {
-    const { playerInfo } = props;
+    const { playerInfo: { owner } } = props;
 
-    const { discards } = usePlayerData(playerInfo.owner);
+    const { discards } = usePlayerData(owner);
 
-    const discardPiles = discards.map((cards,index) =>
-        <DiscardPile key={index} cards={cards} index={index} owner={playerInfo.owner}/>);
-    
+    const discardPiles = discards.map((cards,pileIndex) => {
+        const dropID: CardID = {area:"discardPileAll", pileIndex, owner};
+        const dragID = (cardIndex: number) : CardID => {
+            return {area:"discardPileCard", pileIndex, cardIndex, owner};
+        };
+
+        return <CardStack key={pileIndex} cards={cards} 
+            dropID={dropID}
+            dragID={dragID}/>;
+    });
+
     return <div>
         <PilesDiv> {discardPiles} </PilesDiv>
         <AreaLabelBelow>Discards</AreaLabelBelow>
