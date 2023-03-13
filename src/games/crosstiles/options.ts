f77c5bb42784e473985c241ae97902527e909356
Richard L <richard.latham@yahoo.co.uk>
2023-03-13 15:11:05 +0000

    wip

--- src/games/crosstiles/options.ts
+++ src/games/crosstiles/options.ts
@@ -21,3 +21,102 @@ export const defaultOptions: GameOptions = {
     checkGridBeforeRecoding: true,
     playersGetSameLetters: true,
 }; 
+
+interface BaseOptionDefinition {
+    optionName: string;
+    label: string;
+}
+
+interface BooleanOptionDefinition extends BaseOptionDefinition {   
+    default: boolean; 
+}
+
+interface NumericOptionDefinition extends BaseOptionDefinition {   
+    default: number; 
+    min?: number;
+    max?: number;
+}
+
+export type OptionDefinition = BooleanOptionDefinition | NumericOptionDefinition;
+
+/*
+        <SetNumericOption
+            label="Time to make grid"
+            optionName="timeToMakeGrid"
+
+            options={options}
+            setOptions={setOptions}
+        />
+
+        <SetNumericOption
+            label="Make grid countdown"
+            optionName="makeGridCountdown"
+
+            options={options}
+            setOptions={setOptions}
+        />
+
+        <SetNumericOption
+            label="Rack size"
+            optionName="rackSize"
+            min={6}
+            max={8}
+            
+            options={options}
+            setOptions={setOptions}
+        />
+
+        <SetNumericOption
+            label="Min vowels"
+            optionName="minVowels"
+            min={0}
+            max={2}
+            
+            options={options}
+            setOptions={setOptions}
+        />
+
+        <SetNumericOption
+            label="Min consonsants"
+            optionName="minConsonants"
+            min={0}
+            max={4}
+            
+            options={options}
+            setOptions={setOptions}
+        />
+
+        <SetNumericOption
+            label="Min bonus letters"
+            optionName="minBonusLetters"
+            min={0}
+            max={2}
+            
+            options={options}
+            setOptions={setOptions}
+        />
+
+        <SetBooleanOption
+            label="Players get same letters"
+            optionName="playersGetSameLetters"
+
+            options={options}
+            setOptions={setOptions}
+        />
+
+        <SetBooleanOption
+            label="Warn when recording non-scoring grid"
+            optionName="checkGridBeforeRecoding"
+
+            options={options}
+            setOptions={setOptions}
+        />
+
+        <SetBooleanOption
+            label="Suppress spelling checks (debug)"
+            optionName="checkSpelling"
+
+            options={options}
+            setOptions={setOptions}
+        />
+*/
