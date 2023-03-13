32719f48bcf32f512ba85f68899fbd259b7a027a
Richard L <richard.latham@yahoo.co.uk>
2023-03-13 13:48:16 +0000

    CrossTiles: Add SetBooleanOption

--- src/games/crosstiles/board/set-options.tsx
+++ src/games/crosstiles/board/set-options.tsx
@@ -29,6 +29,34 @@ function parseRestrictedInt(str: string, low?: number, high?: number) : number {
 
     return val;
 }
+interface BooleanOption<Options> {    
+    optionName: keyof Options;
+    options: Options;
+    setOptions: (arg: Options) => void;
+
+    label: string;
+}
+
+function SetBooleanOption<Options>(props: BooleanOption<Options>) {
+    const { options, setOptions, optionName, label } = props;
+
+    const value = options[optionName];
+    sAssert(typeof value === "boolean");
+
+    const toggleValue = () => {
+        // Use 'any' to avoid typescript error due (I assume) to it's not
+        // knowing that newOptions[optionName] is a boolean.
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const newOptions = {...options} as any;
+        newOptions[optionName] = !value;
+        setOptions(newOptions);
+    };
+
+    return <label>
+        {label+" "}
+        <input type="checkbox" checked={value} onChange={toggleValue} />
+    </label>;
+}
 
 interface NumericOption<Options> {    
     optionName: keyof Options;
@@ -46,7 +74,7 @@ function SetNumericOption<Options>(props: NumericOption<Options>) {
     const value = options[optionName];
     sAssert(typeof value === "number");
 
-    const changeValue = (newValue: number) => {
+    const setValue = (newValue: number) => {
         // Use 'any' to avoid typescript error due (I assume) to it's not
         // knowing that newOptions[optionName] is a number.
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -81,7 +109,7 @@ function SetNumericOption<Options>(props: NumericOption<Options>) {
             defaultValue={value}
             onChange={(e: ChangeEvent<HTMLInputElement>) => {
                 const newValue = parseRestrictedInt(e.target.value, min, max);
-                changeValue(newValue);
+                setValue(newValue);
             }}
             min={min}
             max={max}
@@ -154,23 +182,29 @@ function SetOptions(props: SetOptionsProps) {
             setOptions={setOptions}
         />
 
-        <label>{"Players get same letters "}
-            <input type="checkbox" checked={options.playersGetSameLetters}
-                onChange={() => setOptions({...options, playersGetSameLetters: !options.playersGetSameLetters})}
-            />
-        </label>
+        <SetBooleanOption
+            label="Players get same letters"
+            optionName="playersGetSameLetters"
 
-        <label>{"Warn when recording non-scoring grid "}
-            <input type="checkbox" checked={options.checkGridBeforeRecoding}
-                onChange={() => setOptions({...options, checkGridBeforeRecoding: !options.checkGridBeforeRecoding})} 
-            />
-        </label>
+            options={options}
+            setOptions={setOptions}
+        />
 
-        <label>{"Suppress spelling checks (debug) "}
-            <input type="checkbox" checked={!options.checkSpelling}
-                onChange={() => setOptions({...options, checkSpelling: !options.checkSpelling})} 
-            />
-        </label>
+        <SetBooleanOption
+            label="Warn when recording non-scoring grid"
+            optionName="checkGridBeforeRecoding"
+
+            options={options}
+            setOptions={setOptions}
+        />
+
+        <SetBooleanOption
+            label="Suppress spelling checks (debug)"
+            optionName="checkSpelling"
+
+            options={options}
+            setOptions={setOptions}
+        />
 
         <button onClick={() => moves.setOptions(options)}>Set Options</button>
     </SetOptionsDiv>;
