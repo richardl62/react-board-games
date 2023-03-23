02caef3fc5b89f5b6a3d4702434aae0f113160ab
Richard L <richard.latham@yahoo.co.uk>
2023-03-23 18:55:47 +0000

    Lobby: Use InputValues in StartMatch

--- src/app/lobby/start-match.tsx
+++ src/app/lobby/start-match.tsx
@@ -1,27 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { useAsyncCallback } from "react-async-hook";
-import styled from "styled-components";
 import { AppGame } from "../../app-game-support";
 import { loadingOrError, LoadingOrError } from "../../utils/async-status";
 import { BoxWithLegend } from "../../utils/box-with-legend";
 import { createMatch } from "./lobby-tools";
 import { openOnlineMatchPage } from "../url-params";
 import { OfflineOptions } from "../offline-options";
-
-const OuterDiv = styled.div`
-  display: inline-flex;
-  flex-direction: column;
-`;
-
-const OfflineLinkDiv = styled.div`
-  label {
-    margin-left: 8px;
-  }
-
-  input {
-    margin-left: 4px;
-  }
-`;
+import { SpecifiedValues } from "../../app-game-support/value-specification";
+import { InputValues } from "../../app-game-support/value-specification/input-values";
 
 function snapToRange(val: number, low: number, high: number) : number {
     if (val < low) {
@@ -33,7 +19,6 @@ function snapToRange(val: number, low: number, high: number) : number {
     return val;
 }
 
-
 export function StartMatch(props: {
     game: AppGame;
     setOfflineOptions: (opts: OfflineOptions) => void;
@@ -41,58 +26,40 @@ export function StartMatch(props: {
     const { game, setOfflineOptions } = props;
     const {minPlayers, maxPlayers } = game;
 
-    const defaultNumPlayers = snapToRange(2 /*arbitrary*/, minPlayers, maxPlayers);
-
-    const [numPlayers, setNumPlayers] = useState(defaultNumPlayers);
-    const [debugPanel, setDebugPanel] = useState(false);
-
-    const asyncCreateMatch = useAsyncCallback(() =>
-        createMatch(game, numPlayers).then(openOnlineMatchPage)
+    const optionsSpec = {
+        numPlayers: {
+            label: "Number of players",
+            default: snapToRange(2 /*arbitrary*/, minPlayers, maxPlayers),
+            min: minPlayers,
+            max: maxPlayers,
+        },
+        offline: {
+            label: "Play offline (test/debug)",
+            default: false,
+        },
+        debugPanel: {
+            label: "Debug panel (offline only)",
+            default: false,
+        }
+    };
+    
+    const asyncCreateMatch = useAsyncCallback((arg: {numPlayers: number}) =>
+        createMatch(game, arg.numPlayers).then(openOnlineMatchPage)
     );
 
     if(loadingOrError(asyncCreateMatch)) {
         return <LoadingOrError status={asyncCreateMatch} activity="starting match"/>;
     }
 
-    const doSetOfflineOptions = () => setOfflineOptions({
-        numPlayers,
-        debugPanel
-    });
+    const startGame=(options: SpecifiedValues<typeof optionsSpec>) => {
+        if(options.offline) {
+            setOfflineOptions(options);
+        } else {
+            asyncCreateMatch.execute(options);
+        }
+    };
 
-    return <OuterDiv>
-        <BoxWithLegend legend="Start New Game">
-
-            <label htmlFor='numPlayers'>
-                {`Number of players (${minPlayers}-${maxPlayers}):`}
-            </label>
-
-            <input type="number" name='numPlayers'
-                min={minPlayers} max={maxPlayers}
-                value={numPlayers}
-                onChange={(event) => setNumPlayers(Number(event.target.value))}
-            />
-            <button type="button" onClick={asyncCreateMatch.execute} disabled={asyncCreateMatch.loading}>
-                New Game
-            </button>
-        </BoxWithLegend>
-
-        <BoxWithLegend legend="Test/Debug">
-
-            <OfflineLinkDiv>
-                <label>
-                    Debug Panel
-                    <input
-                        type="checkbox"
-                        value={debugPanel ? 1 : 0}
-                        onChange={() => { setDebugPanel(!debugPanel); }}
-                    />
-                </label>
-                <button type="button" onClick={doSetOfflineOptions} >
-                    Offline Game
-                </button>
-            </OfflineLinkDiv>
-
-        </BoxWithLegend>
-
-    </OuterDiv>;
+    return <BoxWithLegend legend="Start New Game">
+        <InputValues specification={optionsSpec} setValues={startGame} />
+    </BoxWithLegend>;
 }
