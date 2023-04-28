ff73a10e1e281772c7eac30a54fdf89242ad93aa
Richard L <richard.latham@yahoo.co.uk>
2023-04-28 19:08:01 +0100

    Dice: Move held dice to start before roll

--- src/games/dice/server-side/roll.ts
+++ src/games/dice/server-side/roll.ts
@@ -5,11 +5,24 @@ export function roll(
     { G, random }: MoveArg0<ServerData>,
     _arg: void,  
 ): void {
-    const faces = G.faces;
-    for (let i = 0; i < faces.length; i++) {
-        if (!G.held[i]) {
-            faces[i] = random.Die(6);
+    // Move held dice to start before rolling
+    const newFaces = [];
+    const newHeld = [];
+    for (let i = 0; i < G.faces.length; i++) {
+        if (G.held[i]) {
+            newFaces.push(G.faces[i]);
+            newHeld.push(true);
         }
     }
+    newFaces.sort();
+    
+    for (let i = newFaces.length; i < G.faces.length; i++) {
+        newFaces.push(random.Die(6));
+        newHeld.push(false);
+    }
+
+    G.faces = newFaces;
+    G.held = newHeld;
+
     G.rollCount++;
 }
