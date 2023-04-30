4dad692e84a4cc6f5015edfef332e6b427290a36
Richard L <richard.latham@yahoo.co.uk>
2023-04-30 18:22:05 +0100

    General: Rework connect and server warnings

--- src/app-game-support/warnings/warnings.tsx
+++ src/app-game-support/warnings/warnings.tsx
@@ -1,16 +1,32 @@
 import React from "react";
-import { ErrorMessage } from "./error-message";
-import { PlayerConnections } from "./player-connections";
-import { ServerConnection } from "./server-connection";
-import { useStandardBoardContext } from "../standard-board";
 
+import { useConnectionWarnings } from "./use-connection-warnings";
+import { useStandardBoardContext } from "../standard-board";
+import styled from "styled-components";
+
+const WarningDiv = styled.div`
+    span:first-child {
+        color: red;
+        font-weight: 600;
+        margin-right: 0.2em;
+    }
+    margin-bottom: 0.2em;
+`;
 
 export function Warnings(): JSX.Element {
     const { G: {moveError} } = useStandardBoardContext();
+
+    const warnings = useConnectionWarnings();
+    if(moveError) {
+        warnings.push("Problem during move " + moveError);
+    }
+    
     return <>
-        <ErrorMessage category="Error during move" message={moveError} />
-        
-        <ServerConnection />
-        <PlayerConnections />
+        {warnings.map((text) => 
+            <WarningDiv key={text}>
+                <span>WARNING: </span>
+                <span>{text}</span>
+            </WarningDiv>
+        )}
     </>;
 }
