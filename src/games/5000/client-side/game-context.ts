4676bd058d8ba61d89627306fc2f120162941acc
Richard L <richard.latham@yahoo.co.uk>
2023-05-29 18:11:31 +0100

    Dice: Wip on dice spin

--- src/games/5000/client-side/game-context.ts
+++ src/games/5000/client-side/game-context.ts
@@ -1,18 +1,38 @@
+import { useEffect, useState } from "react";
 import { useStandardBoardContext } from "../../../app-game-support/standard-board";
 import { WrappedGameProps } from "../../../app-game-support/wrapped-game-props";
 import { ClientMoves } from "../server-side/moves";
 import { ServerData } from "../server-side/server-data";
 
-
 type TypedGameProps = WrappedGameProps<ServerData, ClientMoves>;
 
 interface GameContext extends TypedGameProps {
-    diceSpin: number;
+    diceSpin: number | null;
+}
+
+function useStep(from: number, to: number, step: number, delay: number) : [number | null, () => void]
+{
+    const [value, setValue] = useState(to);
+    useEffect(() => {
+        if (value < to) {
+            const timer = setTimeout(() => setValue(value + step), delay);
+            return () => clearTimeout(timer);
+        }
+    }, [value, to, delay]);
+    
+    return [
+        value < to ? value : null,
+        () => setValue(from),
+    ];
 }
 
 export function useGameContext() : GameContext {
-    const diceSpin = 0;
+    const [diceSpin, start] = useStep(0, 360, 10, 100);
+    if(diceSpin === null) {
+        start();
+    }
+
     const props = useStandardBoardContext() as TypedGameProps;
-    return { ...props, diceSpin };
+    return { ...props, diceSpin  };
 }
 
