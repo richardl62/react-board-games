4cef4f018b006591d4367d3bb3f1e612f2260524
Richard L <richard.latham@yahoo.co.uk>
2023-06-03 09:57:02 +0100

    5000: Hide roll buttons when not players turn

--- src/games/5000/board/player-options.tsx
+++ src/games/5000/board/player-options.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { useGameContext } from "../client-side/game-context";
 import { availablePlayerActions } from "../utils/available-player-actions";
+import styled from "styled-components";
+
+const OuterDiv = styled.div<{visible: boolean}>`
+    visibility: ${props => props.visible ? "visible" : "hidden"};
+`;
 
 export function PlayerOptions() : JSX.Element {
-    const { G, moves } = useGameContext();
-
+    const { G, moves, playerID, ctx: {currentPlayer} } = useGameContext();
+    const isActivePlayer = playerID === currentPlayer;
     const availableActions = availablePlayerActions(G);
     const showRoll = !availableActions.bust;
-    return <div>
+    return <OuterDiv visible={isActivePlayer}>
         {showRoll && <button 
             onClick={() => moves.roll()} 
             disabled={!availableActions.roll}>
@@ -32,5 +37,5 @@ export function PlayerOptions() : JSX.Element {
             Bust
             </button>
         }
-    </div>;
+    </OuterDiv>;
 }
