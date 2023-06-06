63c28979d95135946830f14fab9d0f2c1842cdd1
Richard L <richard.latham@yahoo.co.uk>
2023-06-06 09:34:10 +0100

    5000: Show held dice at start during roll

--- src/games/5000/board/game-dice-set.tsx
+++ src/games/5000/board/game-dice-set.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useGameContext } from "../client-side/game-context";
 import { DiceSet } from "../../../utils/dice/dice-set";
+import { moveHeldFacesToStart } from "../utils/move-held-faces-to-start";
 
 export function GameDiceSet(): JSX.Element {
     const {
-        G: { faces, rollCount, held, turnOverRollCount, }, ctx: { currentPlayer }, playerID, diceRotation, moves
+        G, ctx: { currentPlayer }, playerID, diceRotation, moves
     } = useGameContext();
-    const turnOver = turnOverRollCount === rollCount;
+    const turnOver = G.turnOverRollCount === G.rollCount;
+    const { faces, held } = diceRotation ? moveHeldFacesToStart(G) : G;
 
     let onDiceClick;
     if (!turnOver && playerID === currentPlayer && diceRotation === null) {
--- src/games/5000/server-side/roll.ts
+++ src/games/5000/server-side/roll.ts
@@ -1,36 +1,29 @@
 import { ServerData } from "./server-data";
 import { MoveArg0 } from "../../../app-game-support/bgio-types";
 import { getDiceScores } from "./get-dice-scores";
+import { moveHeldFacesToStart } from "../utils/move-held-faces-to-start";
 
 export function roll(
     { G, random }: MoveArg0<ServerData>,
     _arg: void,  
 ): void {
     // Move held dice to start before rolling
-    const newFaces = [];
-    const newHeld = [];
-    for (let i = 0; i < G.faces.length; i++) {
-        if (G.held[i]) {
-            newFaces.push(G.faces[i]);
-            newHeld.push(true);
+    const {faces, held} = moveHeldFacesToStart(G);
+    for (let i = 0; i < faces.length; i++) {
+        if(!held[i]) {
+            faces[i] = random.Die(6);
         }
     }
-    newFaces.sort();
-    
-    for (let i = newFaces.length; i < G.faces.length; i++) {
-        newFaces.push(random.Die(6));
-        newHeld.push(false);
-    }
 
-    G.faces = newFaces;
-    G.held = newHeld;
+    G.faces = faces;
+    G.held = held;
 
     // Calculate the score from the dice
     G.diceScores = {
         prevRollHeld: G.diceScores.held,
-        ...getDiceScores(G.faces, G.held),
+        ...getDiceScores(faces, held),
     };
-
 
     G.rollCount++;
 }
+
