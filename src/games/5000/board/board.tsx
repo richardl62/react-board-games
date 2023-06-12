8d93e439c33b9b32f3b09be79b937ffc99bd08ac
Richard L <richard.latham@yahoo.co.uk>
2023-06-12 15:29:04 +0100

    Dice: Optionally show scoring combinations

--- src/games/5000/board/board.tsx
+++ src/games/5000/board/board.tsx
@@ -4,11 +4,22 @@ import { GameButtons } from "./game-buttons";
 import { ScorePads } from "./score-pads";
 import styled from "styled-components";
 import { GameDiceSet } from "./game-dice-set";
+import { ScoringCombinations } from "./scoring-combinations";
 
 const OuterDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    // Do not stretch content
+    align-items: flex-start;
+
     padding: 10px;
+
+    a {
+        font-size: 18px;
+    }
 `;
 
+const rulesDoc = "https://docs.google.com/document/d/154dxhfUP_---wevKJ9IqzHMG9XLPxKbJWsoA3Vbi--Y/edit?usp=sharing";
 function Board() : JSX.Element {
  
     return <OuterDiv>
@@ -16,6 +27,8 @@ function Board() : JSX.Element {
         <GameDiceSet/>
         <Scores/>
         <ScorePads />
+        <ScoringCombinations/>
+        <a href={rulesDoc} target="blank">Rules (work in progress)</a>
     </OuterDiv>;
 }
 
