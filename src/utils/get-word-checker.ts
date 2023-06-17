82d6ebb35c5193404400462efdabf3c5c93dce07
Richard L <richard.latham@yahoo.co.uk>
2023-06-17 14:08:39 +0100

    Utils: Reword get-word-checker.ts

--- src/utils/get-word-checker.ts
+++ src/utils/get-word-checker.ts
@@ -1,28 +1,22 @@
 const wordListURL = "https://raw.githubusercontent.com/richardl62/scrabble-words/main/legal-words.txt";
 
-type WordChecker = (word: string) => boolean;
-
-function makeWordChecker(wordList: string) : WordChecker {
-    const legalWords = wordList.split("\n");
-    
-    return (word: string) => {
-        const revisedWord = word.trim().toLocaleLowerCase();
-
-        // KLUDGE: For reasons I don't understand legalWords.includes("")
-        // Returns true
-        return revisedWord !== "" && legalWords.includes(revisedWord);
-    };
+export async function getLegalWordList(): Promise<string[]> {
+    const response = await fetch(wordListURL);
+    if(!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+    }
+    return (await response.text()).split("\n");
 }
 
 /**
  * @returns a promise of a function that checks if a word is legal in Scrabble
  */
-export async function getWordChecker(): Promise<WordChecker> {
-    const response = await fetch(wordListURL);
-    if(!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`);
-    }
-    const text = await response.text();
-    return makeWordChecker(text);
-}
+export async function getWordChecker(): Promise<(word: string) => boolean> {
+    const legalWords = await getLegalWordList();
 
+    return (word: string) => {
+        const revisedWord = word.trim().toLocaleLowerCase();
+
+        return legalWords.includes(revisedWord);
+    };
+}
