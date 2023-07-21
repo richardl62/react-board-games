96aca484979e781e5c563105df6cf7cde18f231d
Richard L <richard.latham@yahoo.co.uk>
2023-07-21 18:23:29 +0100

    Scrabble: Use checkbox for available word

--- src/games/scrabble/board/available-words.tsx
+++ src/games/scrabble/board/available-words.tsx
@@ -1,50 +1,64 @@
 import React from "react";
 import { useScrabbleContext } from "../client-side/scrabble-context";
 import { LetterSet } from "../../../utils/word-finder/letter-set";
+import { Rack } from "../client-side/board-and-rack";
+import { Trie } from "../../../utils/word-finder/trie";
+
+function getAvailableWords(rack: Rack, legalWords: Trie) {
+    let letters = "";
+    let nWildcards = 0;
+    for (const tile of rack) {
+        if (tile === "?") {
+            nWildcards++;
+        } else if (tile) {
+            letters += tile;
+        }
+    }
+
+    const words = legalWords.findWords(new LetterSet(letters, nWildcards));
+
+    if (words.length === 0) {
+        return ["No words found"];
+    }
+
+    // Return words sorted by length, longest first, and uppercase.
+    return words.sort((a, b) => b.length - a.length).map((word) => word.toUpperCase());
+}
+
+function sameRack(rack1: Rack, rack2: Rack) {
+    return rack1.join() === rack2.join();
+}
 
 export function AvailableWords(): JSX.Element | null {
     const {rack, legalWords, reviewGameHistory } = useScrabbleContext();
 
-    type State = {rackLetters: string, words: string[]}
-    const [{words, rackLetters: recordedRackLetters}, setState] = React.useState<State>({words: [], rackLetters: ""});
+    const [showAvailableWords, setShowAvailableWords] = React.useState(false);
+    const [wordsToShow, setWordsToShow] = 
+        React.useState<{words: string[], generatingRack: Rack}>({words: [], generatingRack: []});
 
-    const currentRackLetters = rack.join("");
-    if(recordedRackLetters !== currentRackLetters) {
-        setState({words: [], rackLetters: currentRackLetters});
-    }
-
-    //KLUDGE?: Make available words available only when reviewing game history.
-    if(!reviewGameHistory) {
+    if (!reviewGameHistory) {
         return null;
     }
+    
+    if(!sameRack(rack, wordsToShow.generatingRack)) {
+        const words = showAvailableWords ? getAvailableWords(rack, legalWords) : [];
+        setWordsToShow({words, generatingRack: rack});
+    }
 
-    const getWords = () => {
-        let letters = "";
-        let nWildcards = 0;
-        for(const tile of rack) {
-            if (tile === "?") {
-                nWildcards++;
-            } else if (tile) {
-                letters += tile;
-            }
-        }
-        
-        const words = legalWords.findWords(new LetterSet(letters, nWildcards));
-
-        if (words.length === 0) {
-            return ["No words found"];
-        }
-
-        // Return words sorted by length, longest first, and uppercase.
-        return words.sort((a, b) => b.length - a.length).map((word) => word.toUpperCase());
+    const onCheckBoxChange = () => {
+        setShowAvailableWords(!showAvailableWords);
+        setWordsToShow({words:[], generatingRack:[]}); // Force recalculation of wordsToShow
     };
 
-    const onButtonClick = () => {
-        const words = getWords();
-        setState({words, rackLetters: currentRackLetters});
-    };
     return <div>
-        <button onClick={onButtonClick}>Show Available Words</button>
-        {words.map((word) => <div key={word}>{word}</div>)}
+        <div>
+            <label> {"Show available words "}
+                <input type="checkbox" checked={showAvailableWords}
+                    onChange={onCheckBoxChange}
+                />
+            </label>
+        </div>
+ 
+        {wordsToShow.words.map((word) => <div key={word}>{word}</div>)}
     </div>;
 }
