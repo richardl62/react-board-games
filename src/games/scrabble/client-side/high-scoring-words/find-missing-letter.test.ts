065b25f043fc3905d7365fb93058b3e926585c94
Richard L <richard.latham@yahoo.co.uk>
2023-10-26 10:02:50 +0100

    Scrabble:  Add findMissingLetter

---
+++ src/games/scrabble/client-side/high-scoring-words/find-missing-letter.test.ts
@@ -0,0 +1,25 @@
+import { Trie } from "../../../../utils/word-finder/trie";
+import { findMissingLetter } from "./find-missing-letter";
+
+describe("Find possible letters", () => {
+    const trie = new Trie([
+        "ab", "abc", "adc", "aef", "axcg",
+    ]);
+
+    const testData: [
+            string, // before (i.e. chacters before the one we want to find)
+            string, // after
+            string  // expected result (as string rather than set of characters)  
+        ][] = [
+            ["a","c", "bd"],
+            ["", "b", "a"],
+            ["axc", "", "g"],
+            ["a", "b", ""]
+        ];
+
+    test.each(testData)("%s %s %s", (before, after, expected) => {
+        const found = findMissingLetter({before, after}, trie);
+
+        expect(found.join("")).toEqual(expected);
+    });
+});
---
+++ src/games/scrabble/client-side/high-scoring-words/find-missing-letter.ts
@@ -0,0 +1,29 @@
+import { givenLetter } from "../../../../utils/word-finder/letter-requirement";
+import { LetterSet } from "../../../../utils/word-finder/letter-set";
+import { Trie } from "../../../../utils/word-finder/trie";
+import { WordConstraint } from "../../../../utils/word-finder/word-contraint";
+
+/** Find the letter(s) that can come between 'before' and 'after' to form a word.
+ * ('before' and 'after' can be zero length.)
+ */
+export function findMissingLetter(
+    {before, after} : {before: string, after: string},
+    trie: Trie
+) : string [] // Each string records a single letter
+{
+    const letterRequirements = [
+        ...before.split("").map(givenLetter),
+        null,
+        ...after.split("").map(givenLetter)
+    ];
+
+    const wordConstraint = new WordConstraint(
+        new LetterSet("",1), // 1 wildcard,
+        letterRequirements,
+        letterRequirements.length, // min length
+    );
+
+    const words = trie.findWords(wordConstraint);
+
+    return words.map(w => w[before.length]);
+}
