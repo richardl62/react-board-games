1c57339cc67be863fdf11336eb28bd2d0d3de280
Richard L <richard.latham@yahoo.co.uk>
2023-11-14 09:12:32 +0000

    Scrabble: Change "includes illegal words" message

--- src/games/scrabble/board/score-line.tsx
+++ src/games/scrabble/board/score-line.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { findActiveLetters, getWordsAndScore } from "../client-side";
 import { useScrabbleContext } from "../client-side/scrabble-context";
+import { blank } from "../config";
 
 export function ScoreLine() : JSX.Element | null {
     const context = useScrabbleContext();
 
     const active = findActiveLetters(context);
     const wordsAndScore = getWordsAndScore(context, active);
+    
+    let illegalWords: string[] = [];
+    if(wordsAndScore?.illegalWords) {
+        illegalWords =  wordsAndScore.illegalWords.filter(word => !word.includes(blank));
+    }
 
     if (active.length === 0) {
         return null;
@@ -14,8 +20,9 @@ export function ScoreLine() : JSX.Element | null {
     
     let scoreText = "Word score: ";
     scoreText += wordsAndScore ? wordsAndScore.score : "-";
-    if(wordsAndScore?.illegalWords) {
-        scoreText += " (includes illegal word)";
+    if(illegalWords.length > 0) {
+        const pluralise = illegalWords.length > 1 ? "s" : ""; 
+        scoreText += ` (includes illegal word${pluralise}: ${illegalWords.join(", ")})`;
     }
 
     return <div>{scoreText}</div>;
