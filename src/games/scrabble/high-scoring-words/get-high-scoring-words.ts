82fc490a35ad783880a7f0f69f2e418a8cadace9
Richard L <richard.latham@yahoo.co.uk>
2023-11-18 14:43:47 +0000

    Scrabble: Move getAllWords to new file

--- src/games/scrabble/high-scoring-words/get-high-scoring-words.ts
+++ src/games/scrabble/high-scoring-words/get-high-scoring-words.ts
@@ -1,12 +1,13 @@
-import { LegalWord, getLegalWordsForBoard } from "../../../utils/word-finder/get-legal-words/get-legal-words-for-board";
+import { LegalWord } from "../../../utils/word-finder/get-legal-words/get-legal-words-for-board";
 import { WordDirection } from "../../../utils/word-finder/get-legal-words/word-position";
 import { LetterSet } from "../../../utils/word-finder/letter-set";
 import { Trie } from "../../../utils/word-finder/trie";
 import { BoardAndRack } from "../client-side/board-and-rack";
 import { wordScore } from "../client-side/get-words-and-score";
 import { Letter, ScoringConfig, blank } from "../config";
+import { getAllWords } from "./get-all-words";
 
-function makeLetterSet(letters: (Letter|null)[]) {
+export function makeLetterSet(letters: (Letter|null)[]) {
     let allLetters = "";
     let nBlanks = 0;
     for(const letter of letters) {
@@ -18,41 +19,6 @@ function makeLetterSet(letters: (Letter|null)[]) {
     }
 
     return new LetterSet(allLetters, nBlanks);
-}
-
-function isEmptyBoard(board: (Letter|null)[][]) {
-    const result = board.flat().findIndex((l) => l !== null) < 0;
-    return result;
-}
-
-function getWordsForEmptyBoard(board: (Letter|null)[][], letterSet: LetterSet, trie: Trie) {
-    const words = trie.findWords(letterSet);
-    
-    const row = Math.trunc(board.length/2);
-    const rowLength = board[0].length;
-    const midCol = Math.trunc(rowLength/2);
-
-    const result: LegalWord[] = [];
-    for(let col = 0; col <= midCol; ++col) {
-        for(const word of words) {
-            const wordEnd = col + word.length - 1;
-            if(wordEnd >= midCol && wordEnd < rowLength) {
-                result.push({row, col, direction:"row", word});
-            }
-        }
-    }
-
-    return result;
-}
-
-function getAllWords(br: BoardAndRack, trie: Trie) : LegalWord[] {
-    const board = br.getBoardLetters();
-    const letterSet = makeLetterSet(br.getRack());
-    const words = isEmptyBoard(board) ?
-        getWordsForEmptyBoard(board, letterSet, trie) :
-        getLegalWordsForBoard(board, letterSet, trie);
-
-    return words;
 }
 
 export interface LegalWordAndScore extends LegalWord {
