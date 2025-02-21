0b71367a5070634c01f08f609df1ee15281f4566
Richard L <richard.latham@yahoo.co.uk>
2025-02-21 16:48:57 +0000

    Simplify definition of Ctx

--- src/boardgame-lib/ctx.ts
+++ src/boardgame-lib/ctx.ts
@@ -1,14 +1,7 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 export interface Ctx {
     numPlayers: number;
     playOrder: string[];
     currentPlayer: string;
     playOrderPos: number;
     gameover?: boolean;
-
-    // KLUDGE: This is a hack to help with defining game moves.
-    activePlayers: any;
-    turn: any;
-    phase: any;
 }
