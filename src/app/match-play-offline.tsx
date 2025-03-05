e7caa58a85c13bea5f9e964d0f8e19d56e30b304
Richard L <richard.latham@yahoo.co.uk>
2025-03-05 15:52:53 +0000

    Reworkk MatchPlayOfflne This seems to reduce re-rendering although I don't really understand why.

--- src/app/match-play-offline.tsx
+++ src/app/match-play-offline.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { BoardProps } from "../boardgame-lib/board-props";
 import { AppGame } from "../app-game-support";
 import { GameBoard } from "./game-board";
 import { OfflineOptions } from "./offline-options";
@@ -11,6 +10,21 @@ const OptionalDisplay = styled.div<{display_: boolean}>`
     display: ${props => props.display_? "block" : "none"};
 `;
 
+type SharedData = ReturnType<typeof useSharedOfflineBoardData>;
+
+function Board({game, sharedData, id, show}: {
+    game: AppGame,
+    sharedData: SharedData,
+    id: number,
+    show: boolean,
+}): JSX.Element {
+    const boardProps = offlineBoardProps(game, sharedData, id);
+
+    return <OptionalDisplay display_={show}>
+        <GameBoard game={game} bgioProps={boardProps} />
+    </OptionalDisplay>;
+}
+
 export function MatchPlayOffline(props: {
     game:AppGame,
     options: OfflineOptions,
@@ -21,24 +35,19 @@ export function MatchPlayOffline(props: {
         options: {numPlayers, passAndPlay,  setupData}
     } = props;
 
-    const sharedBoardProps = useSharedOfflineBoardData({game, numPlayers, setupData});
-
-    // Create a board that is optionally displayed. (Early code created either a board
-    // or a blank element. However, this caused the Scrabble dictionary to be reloaded 
-    // on each move. Presumably, this was because the compoment was unloaded and reloaded
-    // each time.)
-    const OptionalBoard = (obProps: BoardProps) => {
-        const show = !passAndPlay || obProps.playerID === obProps.ctx.currentPlayer;
-        return <OptionalDisplay display_={show}>
-            <GameBoard game={game} bgioProps={obProps} />
-        </OptionalDisplay>;
-    };
+    const sharedData = useSharedOfflineBoardData({game, numPlayers, setupData});
 
     const games : JSX.Element[] = [];
     for(let id = 0; id < numPlayers; ++id) {
-        const boardProps = offlineBoardProps(game, sharedBoardProps, id);
-        games[id] = <OptionalBoard  key={id} {...boardProps} />; 
+        // Create a board that is optionally displayed. (Early code created either a board
+        // or a blank element. However, this caused the Scrabble dictionary to be reloaded 
+        // on each move. Presumably, this was because the compoment was unloaded and reloaded
+        // each time.)
+        const show = !passAndPlay || id.toString() === sharedData.ctx.currentPlayer;
+
+        games.push(<Board key={id} game={game} sharedData={sharedData} id={id} show={show} />); 
     }
+
     return (
         <div>{games}</div> 
     );
