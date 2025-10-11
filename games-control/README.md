3f153b23c5bb9f331b5b1424b153e2e0e17a4480
Richard L <richard.latham@yahoo.co.uk>
2025-10-11 09:02:06 +0100

    Wip on new game-control folder

---
+++ games-control/README.md
@@ -0,0 +1,2 @@
+Provide code for controlling games, e.g. move functions and function to provide
+the starting state.
---
+++ games-control/app-games-no-board.ts
@@ -0,0 +1,5 @@
+import { appGameNoBoard as plusMinus } from "./plus-minus/app-game-no-board";
+
+export const gamesNoBoard : Array<AppGameNoBoard> = [
+    plusMinus,
+];
