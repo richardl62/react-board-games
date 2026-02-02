6ea3a9e20761ac1b1f7287d559ca7c41cb44f198
Richard L <richard.latham@yahoo.co.uk>
2026-02-02 17:35:55 +0000

    Changes to connection and other warnings

--- client/app-game-support/warnings.tsx
+++ client/app-game-support/warnings.tsx
@@ -3,45 +3,37 @@ import { JSX, useEffect, useState } from "react";
 import { useStandardBoardContext } from "./standard-board";
 import styled from "styled-components";
 import { getPlayerStatus } from "./player-status";
+import { PublicPlayerMetadata } from "@shared/lobby/types";
+import { Ctx } from "@shared/game-control/ctx";
+import { MatchStatus } from "./board-props";
+import { ConnectionStatus } from "@/app/match-play/online/use-server-connection";
 
 const WarningDiv = styled.div`
     span:first-child {
-        color: red;
+        color: darkred;
         font-weight: 600;
         margin-right: 0.2em;
     }
     margin-bottom: 0.2em;
 `;
 
-// Return true at the given interval after a call in which flag is true.
-// Later calls in which flag is still true have no effect. Later calls with
-// the flag false (or with a different interval) reset the timer. 
-function useDelayedValue(value: boolean, delay: number) {
-  const [delayedValue, setDelayedValue] = useState(false);
+// Return the value if it has been unchanged for the given interval, otherwise return null. 
+function useStableValue<ValueT>(value: ValueT, waitTime: number): ValueT | null {
+    const [settledValue, setSettledValue] = useState<ValueT>(value);
 
-  useEffect(() => {
-    if (!value) {
-      setDelayedValue(false);
-      return;
-    }
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setSettledValue(value);
+        }, waitTime);
+        return () => clearTimeout(handler);
+    }, [value, waitTime]);
 
-    const handler = setTimeout(() => {
-        setDelayedValue(true);
-    }, delay);
-
-    // Run when value or delay changes
-    return () => {
-      clearTimeout(handler);
-    };
-
-  }, [value, delay]);
-
-  return delayedValue;
+    return value === settledValue ? settledValue : null;
 }
 
 // In general return the given text. But to avoid jitters return any non-null
-// text for the given duration even if the input changes.
-function useStableText(text: string | null, minDuration: number): string | null {
+// text for the given duration even if the input changes. (Code by GitHub Copilot.)
+function useJitterFreeText(text: string | null, minDuration: number): string | null {
     const [stableText, setStableText] = useState<string | null>(text);
     const [lastChangeTime, setLastChangeTime] = useState<number>(Date.now());
 
@@ -74,66 +66,92 @@ function useStableText(text: string | null, minDuration: number): string | null 
     return stableText;
 }
 
-function useWarnings() : {
-    connectionIssue: string | null,
-    disconnectedPlayers: string[],
-    errorInLastAction: string | null,
-    lastActionIgnored: boolean,
-} {
-    const {
-        matchStatus: { 
-            connectionStatus, 
-            errorInLastAction, 
-            actionRequestStatus: { lastActionIgnored, waitingForServer },
-            playerData
-        }, 
-        ctx,
 
-    } = useStandardBoardContext();
-
-    // To avoid jitters have a short delay before reporting server delays;
-    const reportServerDelay = useDelayedValue(waitingForServer, 500);
-
-    let connectionIssue : string | null = null;
-    if ( connectionStatus !== "connected" ) {
-        if ( connectionStatus === "connecting" ) {
-            connectionIssue = "Connecting to server...";
-        } else {
-            const reason = connectionStatus.closeEvent.reason || `closure code ${connectionStatus.closeEvent.code}`;
-            connectionIssue = `No connection to server (${reason})`;
-            if (connectionStatus.reconnecting) {
-                connectionIssue += ": attempting reconnection ...";
-            }
+function connectionIssueDescription(
+    connectionStatus: ConnectionStatus,
+    waitingForServer: boolean,  
+): string | null {
+    if (connectionStatus === "connected") {
+        return waitingForServer ? "Waiting for server..." : null;
+    } else if (connectionStatus === "connecting") {
+        return "Connecting to server...";
+    } else {
+        const reason = connectionStatus.closeEvent.reason || `closure code ${connectionStatus.closeEvent.code}`;
+        let connectionIssue = `No connection to server (${reason})`;
+        if (connectionStatus.reconnecting) {
+            connectionIssue += ": attempting reconnection ...";
         }
-    } else if (reportServerDelay) {
-        connectionIssue = "Waiting for server...";
-    }
-    
-    const disconnectedPlayers: string[] = [];
-    for (const pid of ctx.playOrder) {
-        const status = getPlayerStatus(playerData, pid);
-
-        // Warn only about players that have joined but are not now connected
-        if (status.connectionStatus === "not connected") {
-            disconnectedPlayers.push(status.name);
-        }
-    }
-
-    return {
-        connectionIssue,
-        disconnectedPlayers,
-        errorInLastAction,
-        lastActionIgnored,
-    };
+        return connectionIssue;
+    }   
 }
 
-function OptionalWarning({text} : {text: string | null}): JSX.Element {
-    if (!text) {
+// If there is a persistent problem with connection or a move has been ignored,
+// return a string describing the issue otherwise return return null.
+function useConnectionIssue(
+    matchStatus: MatchStatus,
+    acceptableWait: number // milliseconds 
+) : {
+    connectionWarning: string | null,
+    lastActionWarning: string | null,
+} {
+    const {connectionStatus, actionRequestStatus: {waitingForServer, lastActionIgnored}} = matchStatus;
+
+    const stableWaitingForServer = useStableValue( waitingForServer, acceptableWait);
+    const stableConnectionStatus = useStableValue( connectionStatus, acceptableWait);
+
+    let connectionWarning: string | null = null;
+    let lastActionWarning: string | null = null;
+
+    if (stableWaitingForServer  || stableConnectionStatus || lastActionIgnored) {
+        // In general, don't report a transient wait for the server.  But if a move has been ignored
+        // do report it as it helps explain why.
+        const reportWaitingForServer = stableWaitingForServer === true || ( waitingForServer && lastActionIgnored);
+
+        connectionWarning = connectionIssueDescription(connectionStatus, reportWaitingForServer);
+        lastActionWarning = lastActionIgnored ? "Last action was ignored" : null;
+    }
+
+    return { connectionWarning, lastActionWarning };
+}
+
+// If any players are disconnected return a string that names them. Or return null 
+// all players are connected.
+function usePlayersWarning(
+    playerData: PublicPlayerMetadata[],
+    ctx: Ctx,
+    acceptableWait: number // milliseconds 
+) : string | null
+{
+    const warning = () => {
+        const disconnectedPlayers: string[] = [];
+        for (const pid of ctx.playOrder) {
+            const status = getPlayerStatus(playerData, pid);
+
+            // Warn only about players that have joined but are not now connected
+            if (status.connectionStatus === "not connected") {
+                disconnectedPlayers.push(status.name);
+            }
+        }
+
+        if (disconnectedPlayers.length === 0) {
+            return null;
+        }
+        return `${disconnectedPlayers.join(", ")} ${disconnectedPlayers.length === 1 ? "is" : "are"} not connected.`;
+    };
+
+    return useStableValue(warning(), acceptableWait);
+}
+
+
+function Warning({text, minDisplayTime} : 
+    {text: string | null, minDisplayTime: number}): JSX.Element {
+    const stableText = useJitterFreeText(text, minDisplayTime);
+    if (!stableText) {
         return <></>;
     }
     return <WarningDiv>
         <span>WARNING: </span>
-        <span>{text}</span>
+        <span>{stableText}</span>
     </WarningDiv>;
 }
 
@@ -141,28 +159,21 @@ function OptionalWarning({text} : {text: string | null}): JSX.Element {
 // (Individual games should handle warnings about game-specific issues 
 // such as illegal moves.)
 export function Warnings(): JSX.Element {
-    const {
-        connectionIssue,
-        disconnectedPlayers,
-        errorInLastAction,
-        lastActionIgnored,
-    } = useWarnings();
+    const networkIssueWait = 4000; // milliseconds - wait before reporting a possibly transient network issue.
+    const minWarningDisplayTime = 2000; // milliseconds - once a warning is displayed, display it for at least this long.
 
-    const disconnectedMessage = disconnectedPlayers.length === 0 ? null :
-        `${disconnectedPlayers.join(", ")} ${disconnectedPlayers.length === 1 ? "is" : "are"} not connected.`;
-    
-    const lastActionIgnoredText = lastActionIgnored ? "Last move was ignored by the server." : null;
-    const errorInLastActionText = errorInLastAction ? `Error in last action: ${errorInLastAction}` : null;
+    const { matchStatus, ctx } = useStandardBoardContext();
 
-    const stableConnectionIssue = useStableText(connectionIssue, 3000);
-    const stableDisconnectedMessage = useStableText(disconnectedMessage, 3000);
-    const stableLastActionIgnored = useStableText(lastActionIgnoredText, 3000);
-    const stableErrorInLastAction = useStableText(errorInLastActionText, 3000);
+    const {connectionWarning, lastActionWarning} = useConnectionIssue(matchStatus, networkIssueWait);
+    const playersWarning = usePlayersWarning(matchStatus.playerData, ctx, networkIssueWait);
+
+    const { errorInLastAction } = matchStatus;
+    const errorInActionWarning = errorInLastAction ? `Error in last action: ${errorInLastAction}` : null;
 
     return <div>
-        <OptionalWarning text={stableConnectionIssue} />
-        <OptionalWarning text={stableDisconnectedMessage} />
-        <OptionalWarning text={stableLastActionIgnored} />
-        <OptionalWarning text={stableErrorInLastAction} />
+        <Warning text={connectionWarning} minDisplayTime={minWarningDisplayTime} />
+        <Warning text={lastActionWarning} minDisplayTime={minWarningDisplayTime} />
+        <Warning text={playersWarning} minDisplayTime={minWarningDisplayTime} />
+        <Warning text={errorInActionWarning} minDisplayTime={minWarningDisplayTime} />
     </div>;
 }
