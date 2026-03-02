3d19d606ac15a0d03a1b2b7498af7003ccc8d125
Richard L <richard.latham@yahoo.co.uk>
2026-03-02 15:41:33 +0000

    Cannot stop: Remove board text

--- client/app-games/cant-stop/board/board.tsx
+++ client/app-games/cant-stop/board/board.tsx
@@ -1,13 +1,9 @@
 import { JSX } from "react";
 import { TurnControl } from "./turn-control";
 import { Columns } from "./columns";
-import { BoardText } from "./board-text/board-text";
 
 function Board() : JSX.Element {
-    
-    // Very crude board for now.
     return <div>
-        <BoardText/>
         <Columns/>
         <TurnControl/>
     </div>
