99c2b65a7afbfa05bf60b594dd60a6837acf5510
Richard L <richard.latham@yahoo.co.uk>
2026-03-02 16:09:50 +0000

    Display players' names

--- client/app-games/cant-stop/board/board.tsx
+++ client/app-games/cant-stop/board/board.tsx
@@ -1,12 +1,22 @@
 import { JSX } from "react";
 import { TurnControl } from "./turn-control";
 import { Columns } from "./columns";
+import { PlayerNames } from "./player-names";
+import styled from "styled-components";
+
+const BoardContainer = styled.div`
+    display: inline-flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1em;
+`;
 
 function Board() : JSX.Element {
-    return <div>
+    return <BoardContainer>
+        <PlayerNames/>
         <Columns/>
         <TurnControl/>
-    </div>
+    </BoardContainer>
 }
 
 export default Board;
---
+++ client/app-games/cant-stop/board/player-names.tsx
@@ -0,0 +1,29 @@
+import { JSX } from "react";
+import { useMatchState } from "../match-state/match-state";
+import styled from "styled-components";
+import { playerColor } from "./styles";
+
+const Names = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+const Name = styled.div<{color: string}>`
+    color: ${props => props.color};
+    font-size: 1.5em;
+    font-weight: bold;
+`
+export function PlayerNames() : JSX.Element {
+    const { ctx, getPlayerName } = useMatchState();
+
+    return <Names>
+        {ctx.playOrder.map((pid) =>
+            <Name key={pid} color={playerColor(pid)}>
+                {getPlayerName(pid)}
+            </Name>
+        )}
+
+    </Names>
+}
