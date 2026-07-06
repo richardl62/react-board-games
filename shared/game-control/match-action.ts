b87f93d74e365f98ad23965ed9c81c75edfe7059
Richard L <richard.latham@yahoo.co.uk>
2026-07-06 19:24:05 +0100

    Change to include path to fix build breakage I am not sure when the problem arose

--- shared/game-control/match-action.ts
+++ shared/game-control/match-action.ts
@@ -4,7 +4,7 @@ import { RandomAPI } from '../utils/random-api.js';
 import { Ctx, endMatch, endTurn } from './ctx.js';
 import { AllActive, GameControl } from './game-control.js';
 import { MoveArg0, getMoveFunction, isOutOfSequenceMove } from './move-fn.js';
-import { sAssert } from '@shared/utils/assert.js';
+import { sAssert } from '../utils/assert.js';
 
 // The result of a move: the new ActiveMatchState plus the updated per-player data
 // (a clone of the input playerData with any setPlayerData mutations applied).
